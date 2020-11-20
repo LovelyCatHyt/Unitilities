780125a568b5fe1160b81dd0f6028c3710654813
Hyt <69683382@qq.com>
2020-11-20 18:25:22 +0800

    Add Effect module
    Now only support a ShowUp Effect

---
+++ Effect/IEffect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Unitilities.Effect
+{
+    /// <summary>
+    /// 特效
+    /// </summary>
+    public interface IEffect
+    {
+        // 播放
+        void Play();
+        // 强制停止
+        void Stop();
+        // 添加结束监听器, 在正常结束或强制停止时调用
+        void AddFinishListener(Action action);
+    }
+}
---
+++ Effect/ShowUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unitilities.Effect
+{
+
+    public class ShowUp : MonoBehaviour, IEffect
+    {
+        [SerializeField] private Transform target;
+        private event Action finish;
+        float usedTime;
+        private IEnumerator runningLoop;
+        public float effectTime = 0.5f;
+        public Vector3 targetScale = Vector3.one;
+
+        private IEnumerator Loop()
+        {
+            for(usedTime = 0f; usedTime < effectTime; usedTime += Time.deltaTime)
+            {
+                target.localScale = Vector3.Lerp(Vector3.zero, targetScale,
+                    MathTool.SpeedUpLerp(0, 1, usedTime / effectTime));
+                yield return null;
+            }
+            OnFinish();
+        }
+
+        [ContextMenu("Play")]
+        public void Play()
+        {
+            StartCoroutine(runningLoop = Loop());
+        }
+
+        [ContextMenu("Stop")]
+        public void Stop()
+        {
+            StopCoroutine(runningLoop);
+            target.localScale = targetScale;
+            OnFinish();
+        }
+
+        public void AddFinishListener(Action action)
+        {
+            finish += action;
+        }
+
+        protected virtual void OnFinish()
+        {
+            target.localScale = targetScale;
+            finish?.Invoke();
+        }
+    }
+
+}
